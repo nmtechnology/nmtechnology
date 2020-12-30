9b938052201f8d6cd038a666919a9268a9447fca
Sliptek <sliptek@Slipteks-iMac.hsd1.nm.comcast.net>
2020-12-30 10:33:10 -0700

    revamp of company site

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en" class="html__responsive img-fluid">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/png" href="img/NM Technology-favicon.png">
+    <!--Bootstrap CSS-->
+    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" rel="stylesheet">
+    <!--Google FOnt-->
+    <link href="https://fonts.googleapis.com/css?family=Saira:200,400&display=swap" rel="stylesheet">
+    <!--CSS stylesheet-->
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <!--aos animation-->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css">
+    <!--AOS Js-->
+    <script type="text/javascript" async="" src="https://www.gstatic.com/recaptcha/releases/v1QHzzN92WdopzN_oD7bUO2P/recaptcha__en.js"></script><script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <!--jQuery first, then Popper.js, then Bootstrap JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <!--jQuery Form, Additional Methods, Validate-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+    <!--Your JavaScript Form Validator-->
+    <script src="js/form-validate.js"></script>
+
+    <!--Google reCAPTCHA-->
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+
+    <!--load amimate.css from CDN-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css">
+
+    <!--load WOW js from CDN-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js">
+    </script>
+
+    <title>NM Technology</title>
+
+</head>
+
+<body data-aos-easing="ease" data-aos-duration="400" data-aos-delay="0" class="img-fluid">
+<!--/**background image**/-->
+<div id="background-image" class="img-fluid">
+    <!--Top banner with NM Technology logo fixed to top-->
+    <header>
+        <nav class="nav bg-transparent position-fixed">
+            <div id="top-banner" alt="nmtis-logo-banner" class="img-fluid position-fixed"></div>
+        </nav>
+    </header>
+
+    <!--**********1st section-->
+    <section>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="container p-0 box-back">
+            <div class="row-5">
+                <div class="container">
+                    <br>
+                    <h1 class="typewriter display-6 p-2 font-weight-light">Welcome to NM Technology!</h1><div class=row-3></div>
+                    <br>
+                    <p class="p-4">When you have a serious technology based project that needs to be installed right,
+                        look no further than NM Technology. NM Technology is a Southwest leader in technology
+                        integration for professional commercial subcrontracting. We deliver structured cabling, network comm 
+                        support and now full-stack web development!<br>
+
+                        <br>NM Technology was founded in 2016 and we enjoy providing our clients and stakeholders
+                        with a world class experience while maintaining our contract or completing an 
+                        installation project from start to finish! We have served many different types of clients and the
+                        list keeps growing day by day! Our team has performed work inside various US government sites and high
+                        security scientific facilities that demand privacy and above all integrity from a technology service provider like
+                        us!<br>
+                    <div>
+                    <br>
+                    <h6>Technology -</h6><p class="p-3">Is an expanded concept that deals with a species' usage and knowledge of tools and
+                        crafts, and how it affects a species' ability to control and adapt to its environment. The human race's
+                        use of technology began with the conversion of plentiful natural resources into simple tools.</p></div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <!--    2nd section FULLSTACK DEV-->
+        <div class="container p-3 mt-5 aos-init aos-animate box-back" data-aos="fade-up">
+            <div class="row-5">
+                <div class="container p-0">
+                    <h2 class="display-4">Fullstack Development</h2>
+                    <div class="container">
+                        <div class="row">
+                            <div class="d-inline-flex justify-content-center container">
+                                <img src="/img/team2.jpg" height="200" width="210" class="img-rounded mb-2" alt="teamwork">
+                                <img src="/img/code.jpg" height="200" width="210" class="img-rounded mb-2" alt="php-code">
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <p class="p-2">NM Technology as a full-stack development company, empowers businesses with a software
+                        tool in order to create an efficient work-flow process and drive up production levels. Cloud-based solutions
+                        that are scalable and reliable. If your company has a product or service you want to market on the world wide 
+                    web we can help you do that! Contact NM Technology below for a free consultation.</p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <!-- 3rd section COMPANIONCLOUD-->
+        <div class="container aos-init aos-animate box-back" data-aos="fade-up">
+            <div class="row-5">
+                <div class="container p-2">
+                    <h2 class="display-4 p-2">NM Technology Projects</h2><img src="../img/CompanionCloudlogo.png" height="82" width="300" class="mb-2 float-center" alt="companion cloud logo">
+                    <div>
+                        <img src="../img/CompanionCloud-ACP.png" height="435" width="157" class="d-block w-100 img-thumbnail" alt="companion cloud screenshot">
+                    </div>
+                    <br>
+                    <p class="p-2">CompanionCloud is a pet cremation tracking web application that enables a pet cremation establishment
+                        manage an efficient workflow while processing customer cremation orders and keeping the owner of the companion informed the entire process. CompanionCloud is a SaaS application and can be used as a tool for order processing, tracking and invoicing within the pet cremation process.
+                        <br> <br>CompanionCloud uses a status based approach for its tracking function and informs staff as to
+                        what stage of processing the pet is currently going through. Pet owners also receive SMS and EMAIL
+                        notifications updates in order to ensure their order needs have been met the entire process from start
+                        to finish.<br> <br>Pet cremation staff, Veterinary clinics and pet owners all fulfill a specific user
+                        role within the CompanionCloud application each with their own permissions. Technicians utilize a mobile
+                        app version of CompanionCloud to manage tracking of the pet profile. CompanionCloud is live and currently
+                        has 24 vet clinics using the system right here in Albuquerque. NM Technology provides administrative monthly
+                        support for CompanionCloud users.</p>
+
+                    <h2>Global Indie Freeform Radio</h2>
+                    <div><!--GlobalINDIEfreeform radio player-->
+                    <script>(function(d, s){if(!window.rel){s = d.createElement("script");s.type = "text/javascript";s.async = true;s.id = "radio-de-embedded";s.src = "https://www.radio.net/inc/microsite/js/full.js";d.getElementsByTagName("head")[0].appendChild(s);window.rel = true;}}(document));</script>
+                        <div class="ng-app-embedded"><div ui-view class="microsite embedded-radio-player" data-playerwidth="340px" data-playertype="web_embedded" data-playstation="globalindiefreeform" data-autoplay="true" data-apikey="df04ff67dd3339a6fc19c9b8be164d5b5245ae93"></div></div><noscript><a href="https://www.radio.net/s/globalindiefreeform" target="_blank">Global Indie FreeForm on radio.net</a></noscript></div>
+                    <p>GIFF Radio is a stakeholder project</p>
+                    <h6>GlobalINDIEFreeform.com</h6>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact form -->
+    <section class="bg-dark mt-3 aos-init aos-animate" data-aos="fade-up">
+        <div class="container">
+            <div class="row">
+                <div class="mt-5">
+                    <form id="pwp-contact-form" action="../php" method="POST" class="form" novalidate="novalidate">
+                        <div>
+                            <div class="container bg-transparent d-inline-flex">
+                                <div class="row container">
+                                    <h6>Fullstack Web Development</h6>
+                                    <ul class="container ml-2">
+                                        <li>Custom Web & Mobile Apps</li>
+                                        <li>Mobile Responsive Website's</li>
+                                        <li>Web Automation</li>
+                                        <li>SMS Marketing</li>
+                                    </ul>
+                                </div>
+                                <div class="row container">
+                                    <h6>Installation Services</h6>
+                                    <ul class="container">
+                                        <li>4k IP-CCTV Installation</li>
+                                        <li>Network Infrastructure</li>
+                                        <li>Structured Cabling</li>
+                                        <li>Access Control</li>
+                                    </ul>
+                                </div>
+                                <div class="row container">
+                                    <h6>Maintenance Services</h6>
+                                    <ul class="container">
+                                        <li>Cable Management</li>
+                                        <li>Hardware Upgrades</li>
+                                        <li>Network Upgrades</li>
+                                        <li>On-site Technician T/S</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div>
+                                <h3 class="ml-5 mt-5 display-6" data-aos-delay="50">What Can We Help You With?</h3>
+                            </div>
+                            <div class="mt-3 container"><label for="name"></label><input id="name" name="name" type="text" class="form-control bg-dark form text-warning row" placeholder="Name*"><br>
+                                <label for="email"></label><input id="email" name="email" type="text" class="form-control bg-dark form text-warning row" placeholder="Email*"></div></div>
+                        <label for="message"></label><textarea id="message" name="message" class="form-control bg-dark text-warning form col mt-4" placeholder="Message*" rows="2"></textarea>
+
+                        <!-- reCAPTCHA -->
+                        <div class="container">
+                            <div class="g-recaptcha mt-4 row" data-sitekey="6LcIcNoUAAAAAPpNZqQDKjGm91xVKw8TkSjKOiew"><div style="width: 304px; height: 78px;"><div><iframe src="https://www.google.com/recaptcha/api2/anchor?ar=1&amp;k=6LcIcNoUAAAAAPpNZqQDKjGm91xVKw8TkSjKOiew&amp;co=aHR0cHM6Ly9ubXRpcy5jb206NDQz&amp;hl=en&amp;v=v1QHzzN92WdopzN_oD7bUO2P&amp;size=normal&amp;cb=q4ego9g0xqld" width="304" height="78" role="presentation" name="a-krqvyqdutfr3" frameborder="0" scrolling="no" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"></iframe></div><textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"></textarea></div><iframe style="display: none;"></iframe></div>
+                        </div>
+                        <!--SUBMIT BUTTON-->
+                        <button type="submit" class="btn mt-4 container p-2" id="submit-button">Send</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!--empty area for form error/success output-->
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <div>
+            <div>
+                <div id="output-area">
+                </div>
+            </div>
+        </div><!-- /.container-->
+
+    </section>
+    <!-- Wrapped background image closing div was here-->
+    <!--Footer-->
+    <div class="container-fluid justify-content-center site-footer--nav" id="footer">
+        <footer>
+            </footer>
+    </div>
+
+
+    <script>
+        AOS.init();
+    </script>
+
+        <div
+            style="visibility: hidden; position: absolute; width: 100%; top: -10000px; left: 0px; right: 0px; transition: visibility 0s linear 0.3s, opacity 0.3s linear 0s; opacity: 100%;">
+        <div
+            style="width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: 2000000000; background-color: rgb(255, 255, 255); opacity: 0.5;"></div>
+        <div
+            style="margin: 0px auto; top: 0px; left: 0px; right: 0px; position: absolute; border: 1px solid rgb(204, 204, 204); z-index: 2000000000; background-color: rgb(255, 255, 255); overflow: hidden;">
+        <iframe title="recaptcha challenge"
+                  src="https://www.google.com/recaptcha/api2/bframe?hl=en&amp;v=v1QHzzN92WdopzN_oD7bUO2P&amp;k=6LcIcNoUAAAAAPpNZqQDKjGm91xVKw8TkSjKOiew&amp;cb=g79avnzpp0d"
+                  name="c-krqvyqdutfr3" frameborder="0" scrolling="no"
+                  sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"
+                  style="width: 100%; height: 100%;"></iframe>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
