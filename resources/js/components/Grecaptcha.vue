eb6e2d9a697afbc282824cb84eaaade4d042ddca
pleyba4@cnm.edu <patrick@nmtechnology.us>
2023-06-25 01:40:12 -0600

    working on layout of homepage

--- resources/js/components/Grecaptcha.vue
+++ resources/js/components/Grecaptcha.vue
@@ -12,19 +12,34 @@ const onloadCallback = function () {
 
 
 
-<template>
-    <div class="content-center flex items-center justify-center bg-blue-950 mx-auto">
-        <div class="bg-blue-950">
+<template><div class="relative isolate">
+    <svg class="absolute inset-x-0 top-0 -z-10 h-[38rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_28rem_at_center,white,transparent)]" aria-hidden="true">
+        <defs>
+            <pattern id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84" width="200" height="50" x="50%" y="-1" patternUnits="userSpaceOnUse">
+
+            </pattern>
+        </defs>
+            <svg x="50%" y="-1" class="overflow-hidden">
+                <path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z" stroke-width="0" />
+            </svg>
+        <rect width="100%" height="60%" stroke-width="4" fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)" />
+    </svg>
+</div>
+    <div class="flex justify-center bg-blue-950">
+        <div class="bg-blue-950 m-40">
+
             <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Let's Get You Verified</p>
-            <p class="text-lg leading-8 flex-auto flex-col text-gray-300">As you can see were all about security, please pass our verification to continue to our site
-                and prove you are a human, thank you!</p>
+            <p class="text-lg leading-8 flex-auto flex-col text-gray-300">As you can see were all about security, please utilize our verification to continue to our site
+                to prove you are human, thank you!</p>
+
             <div class="bg-blue-950">
                 <div class="g-recaptcha" id="g-form" data-sitekey="6LeQOiUaAAAAAKFMZr3FD88EEbGVABvgw4pMYUny" data-callback="enableBtn"></div>
                 <br>
                 <a href="/" class="offset-md-2 offset-lg-2 offset-sm-3">
-                    <input class="disabled cursor-not-allowed rounded-md bg-lime-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    <input class="cursor-not-allowed rounded-md bg-lime-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                            type="button" value="Get Started" data-expired-callback="enableBtn"></a>
             </div>
+
         </div>
     </div>
 
--- resources/js/components/HomePage.vue
+++ resources/js/components/HomePage.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="bg-blue-950">
         <header class="absolute inset-x-0 top-0 z-50">
-            <nav class="mx-auto bg-blue-950 flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+            <nav class="mx-auto bg-blue-950 flex max-w-7xl items-center justify-between p-6 lg:px-8 bg-gradient-to-r from-blue-950" aria-label="Global">
                 <div class="flex lg:flex-1">
                     <a href="#" class="-m-1.5 p-1.5">
                         <span class="sr-only">NM Technology</span>
-                        <img class="h-8 w-auto flex-none" src="/images/nm-logo-rmbg.webp" alt="nmtechnology-logo">
-                    </a><a href="#" class="text-sm -space-x-0 font-bold leading-6 text-white flex-initial inset-0">Technology</a>
+                        <img class="h-8 w-auto flex" src="/images/nm-logo-rmbg.webp" alt="nmtechnology-logo">
+                    </a><a href="#" class="text-sm -space-x-0 font-bold leading-6 text-white flex-initial">Technology</a>
                 </div>
 
                 <div class="flex lg:hidden">
                     <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-skin-base">
                         <span class="sr-only">Open main menu</span>
                         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+<!--                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />-->
                         </svg>
                     </button>
                 </div>
@@ -41,9 +41,9 @@
                         </a><a href="#" class="text-sm italic font-bold leading-6 text-white flex-initial">Technology</a>
                         <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
                             <span class="sr-only">Close menu</span>
-                            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
+<!--                            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">-->
+<!--                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />-->
+<!--                            </svg>-->
                         </button>
                     </div>
                     <div class="mt-6 flow-root">
@@ -53,6 +53,21 @@
                                 <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Security Systems</a>
                                 <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Access Control</a>
                                 <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Web Development</a>
+                            </div>
+                            <div class="mt-16 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
+                                <svg viewBox="0 0 366 729" role="img" class="mx-auto w-[22.875rem] max-w-full drop-shadow-xl">
+                                    <title>App screenshot</title>
+                                    <defs>
+                                        <clipPath id="2ade4387-9c63-4fc4-b754-10e687a0d332">
+                                            <rect width="316" height="684" rx="36" />
+                                        </clipPath>
+                                    </defs>
+                                    <path fill="#4B5563" d="M363.315 64.213C363.315 22.99 341.312 1 300.092 1H66.751C25.53 1 3.528 22.99 3.528 64.213v44.68l-.857.143A2 2 0 0 0 1 111.009v24.611a2 2 0 0 0 1.671 1.973l.95.158a2.26 2.26 0 0 1-.093.236v26.173c.212.1.398.296.541.643l-1.398.233A2 2 0 0 0 1 167.009v47.611a2 2 0 0 0 1.671 1.973l1.368.228c-.139.319-.314.533-.511.653v16.637c.221.104.414.313.56.689l-1.417.236A2 2 0 0 0 1 237.009v47.611a2 2 0 0 0 1.671 1.973l1.347.225c-.135.294-.302.493-.49.607v377.681c0 41.213 22 63.208 63.223 63.208h95.074c.947-.504 2.717-.843 4.745-.843l.141.001h.194l.086-.001 33.704.005c1.849.043 3.442.37 4.323.838h95.074c41.222 0 63.223-21.999 63.223-63.212v-394.63c-.259-.275-.48-.796-.63-1.47l-.011-.133 1.655-.276A2 2 0 0 0 366 266.62v-77.611a2 2 0 0 0-1.671-1.973l-1.712-.285c.148-.839.396-1.491.698-1.811V64.213Z" />
+                                    <path fill="#343E4E" d="M16 59c0-23.748 19.252-43 43-43h246c23.748 0 43 19.252 43 43v615c0 23.196-18.804 42-42 42H58c-23.196 0-42-18.804-42-42V59Z" />
+                                    <foreignObject width="316" height="684" transform="translate(24 24)" clip-path="url(#2ade4387-9c63-4fc4-b754-10e687a0d332)">
+                                        <img src="https://tailwindui.com/img/component-images/mobile-app-screenshot.png" alt="" />
+                                    </foreignObject>
+                                </svg>
                             </div>
                         </div>
                     </div>
@@ -67,7 +82,7 @@
                             <path d="M.5 200V.5H200" fill="none" />
                         </pattern>
                     </defs>
-                    <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
+                    <svg x="50%" y="-1" class="overflow-visible">
                         <path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z" stroke-width="0" />
                     </svg>
                     <rect width="100%" height="100%" stroke-width="0" fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)" />
