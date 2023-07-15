0bab3ac20a9bddf696f8e43dde2de76603a3c48a
NM Technology <nmtechnology@sliptek.hsd1.nm.comcast.net>
2023-07-15 10:07:43 -0600

    formatted vue files correctly

--- resources/js/form-validate.js
+++ resources/js/form-validate.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
     /**
      * jQuery Validate Function
      *
@@ -15,7 +14,6 @@ $(document).ready(function(){
 
     /* begin validate function here */
     $("#contact-form").validate({
-
         // setup handling of form errors
         debug: true,
         errorClass: "alert alert-danger",
@@ -26,41 +24,40 @@ $(document).ready(function(){
         // each rule starts with the form input element's NAME attribute
         rules: {
             name: {
-                required: true
+                required: true,
             },
             email: {
                 email: true,
-                required: true
+                required: true,
             },
             message: {
                 required: true,
-                maxlength: 2000
-            }
+                maxlength: 2000,
+            },
         },
 
         // error messages to display to the end user when rules above don't pass
         messages: {
             name: {
-                required: "Please enter your name!"
+                required: "Please enter your name!",
             },
             email: {
                 email: "**Please enter a valid email address**",
-                required: "Please enter a valid email address."
+                required: "Please enter a valid email address.",
             },
             message: {
                 required: "You need to enter a message!",
-                maxlength: "2000 characters max."
-            }
+                maxlength: "2000 characters max.",
+            },
         },
 
         // AJAX submit the form data to back end if rules pass
-        submitHandler: function(form) {
+        submitHandler: function (form) {
             $("#contact-form").ajaxSubmit({
-
                 type: "POST",
                 url: $("#contact-form").attr("action"),
 
-                success: function(ajaxOutput) {
+                success: function (ajaxOutput) {
                     // clear the output area's formatting
                     $("#output-area").css("display", "");
 
@@ -68,13 +65,11 @@ $(document).ready(function(){
                     $("#output-area").html(ajaxOutput);
 
                     // reset the form if it was successful
-                    if($(".alert-success").length >= 1) {
+                    if ($(".alert-success").length >= 1) {
                         $("#contact-form")[0].reset();
                     }
-                }
-            })
-        }
-
-    });/* end validate function here */
-
-});/*end document.ready()*/
+                },
+            });
+        },
+    }); /* end validate function here */
+}); /*end document.ready()*/
