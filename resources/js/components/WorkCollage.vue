8eb2ef706e65c8cb79f0c4869ebc19c83ce97999
nmtechnology <nmdev@corbindallass-MacBook-Pro.local>
2024-11-15 21:30:41 -0700

    test

--- resources/js/components/WorkCollage.vue
+++ resources/js/components/WorkCollage.vue
@@ -1,4 +1,5 @@
 <template>
+    <h6 class="text-lime-400 text-bold text-center">QSR Franchise Security Systems</h6>
     <div class="container bg-gray-900 mx-auto mb-12 px-5 py-2 lg:px-32 lg:pt-24">
         <div class="-m-1 flex flex-wrap md:-m-2">
             <div class="flex w-1/2 flex-wrap">
@@ -6,7 +7,7 @@
                     <img
                         alt="gallery"
                         class="block h-full w-full rounded-lg object-cover object-center"
-                        src="/public/images/IT-rack2.webp"
+                        src="/public/images/m3125lve_w_weather_shield_2406_hi_Mediaportal - Web PNG.webp"
                     />
                 </div>
                 <div class="w-1/2 p-1 md:p-2">
@@ -20,7 +21,7 @@
                     <img
                         alt="gallery"
                         class="block h-full w-full rounded-lg object-cover object-center shadow-2xl"
-                        src="/public/images/cctv-corner.webp"
+                        src="/public/images/retail-clothing-cctv.webp"
                     />
                 </div>
             </div>
@@ -43,7 +44,113 @@
                     <img
                         alt='gallery'
                         class="block h-full w-full rounded-lg object-cover object-center"
-                        src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
+                        src="/public/images/cctv-corner.webp"
+                    />
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <h1 class="text-bold text-lime-400 text-center">Commercial Retail CCTV </h1>
+        <br>
+        <div class="-m-1 flex flex-wrap md:-m-2">
+            <div class="flex w-1/2 flex-wrap">
+                <div class="w-1/2 p-1 md:p-2">
+                    <img
+                        alt="gallery"
+                        class="block h-full w-full rounded-lg object-cover object-center"
+                        src="/public/images/m3125lve_w_weather_shield_2406_hi_Mediaportal - Web PNG.webp"
+                    />
+                </div>
+                <div class="w-1/2 p-1 md:p-2">
+                    <img
+                        alt="gallery"
+                        class="block h-full w-full rounded-lg object-cover object-center shadow-2xl"
+                        src="/public/images/cctv-highway.webp"
+                    />
+                </div>
+                <div class="w-full p-1 md:p-2">
+                    <img
+                        alt="gallery"
+                        class="block h-full w-full rounded-lg object-cover object-center shadow-2xl"
+                        src="/public/images/retail-clothing-cctv.webp"
+                    />
+                </div>
+            </div>
+            <div class="flex w-1/2 flex-wrap">
+                <div class="w-full p-1 md:p-2">
+                    <img
+                        alt="gallery"
+                        class="block h-full w-full rounded-lg object-cover object-center shadow-2xl"
+                        src="/public/images/cctv-store.webp"
+                    />
+                </div>
+                <div class="w-1/2 p-1 md:p-2">
+                    <img
+                        alt="gallery"
+                        class="block h-full w-full rounded-lg object-cover object-center shadow-2xl"
+                        src="/public/images/cctv-restaurant.webp"
+                    />
+                </div>
+                <div class="w-1/2 p-1 md:p-2">
+                    <img
+                        alt='gallery'
+                        class="block h-full w-full rounded-lg object-cover object-center"
+                        src="/public/images/cctv-corner.webp"
+                    />
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <h1 class="text-bold text-lime-400 text-center">Commercial Retail CCTV </h1>
+        <br>
+        <div class="-m-1 flex flex-wrap md:-m-2">
+            <div class="flex w-1/2 flex-wrap">
+                <div class="w-1/2 p-1 md:p-2">
+                    <img
+                        alt="gallery"
+                        class="block h-full w-full rounded-lg object-cover object-center"
+                        src="/public/images/m3125lve_w_weather_shield_2406_hi_Mediaportal - Web PNG.webp"
+                    />
+                </div>
+                <div class="w-1/2 p-1 md:p-2">
+                    <img
+                        alt="gallery"
+                        class="block h-full w-full rounded-lg object-cover object-center shadow-2xl"
+                        src="/public/images/cctv-highway.webp"
+                    />
+                </div>
+                <div class="w-full p-1 md:p-2">
+                    <img
+                        alt="gallery"
+                        class="block h-full w-full rounded-lg object-cover object-center shadow-2xl"
+                        src="/public/images/retail-clothing-cctv.webp"
+                    />
+                </div>
+            </div>
+            <div class="flex w-1/2 flex-wrap">
+                <div class="w-full p-1 md:p-2">
+                    <img
+                        alt="gallery"
+                        class="block h-full w-full rounded-lg object-cover object-center shadow-2xl"
+                        src="/public/images/cctv-store.webp"
+                    />
+                </div>
+                <div class="w-1/2 p-1 md:p-2">
+                    <img
+                        alt="gallery"
+                        class="block h-full w-full rounded-lg object-cover object-center shadow-2xl"
+                        src="/public/images/cctv-restaurant.webp"
+                    />
+                </div>
+                <div class="w-1/2 p-1 md:p-2">
+                    <img
+                        alt='gallery'
+                        class="block h-full w-full rounded-lg object-cover object-center"
+                        src="/public/images/cctv-corner.webp"
                     />
                 </div>
             </div>
