aa5ec57281b073d9a92f5010feb9f31cc15f6d7c
nmtechnology <nmtechnology@nmtechnologys-iMac.local>
2025-01-12 21:30:37 -0700

    test push

--- resources/js/components/TopBanner.vue
+++ resources/js/components/TopBanner.vue
@@ -9,7 +9,7 @@
     <div class="flex gap-x-4 gap-y-2 justify-center">
       <img src="/public/images/usa-flag.webp" alt="" class="h-8 w-auto animate-pulse" />
       <p class="text-sm leading-6 text-white">
-        <strong class="font-semibold">Fall Tech Sale!! Save 15%, call or email us!</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="5" cy="5" r="5" /></svg>Fall Sale Ends 11/30
+        <strong class="font-semibold">+1(505)373-7066 call or email us!</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="5" cy="5" r="5" /></svg>
       </p>
     
       <a href="" class="rounded-xl bg-green-700 px-3.5 py-1 text-sm font-semibold flex text-gray-900 shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900 text-center">CALL TODAY!<span aria-hidden="true"></span></a>
