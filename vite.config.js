5a8b8bcdf2c1d113a129df45c08401257d9e6b50
nmtechnology <patrick@nmtechnology.us>
2025-09-05 19:55:57 -0600

    Update image paths and Vite config for assets
    Changed image paths from '/public/images/...' to '/images/...' in HomePage.vue and LandingPage.vue for consistency and correct asset referencing. Updated vite.config.js to mark '/public/images/zia-symbol.png' as external in Rollup options.

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+            external: ['/public/images/zia-symbol.png']
+        }
+    },
 });
