74c04fabe8b6b61f108d89a760b0806d7caa97b2
nmtechnology <patrick@nmtechnology.us>
2025-09-14 17:29:16 -0600

    Adjust homepage banner margin and scroll threshold
    Changed the top margin of the homepage title from 20 to 10 and updated the scroll threshold for showing the fixed banner from 40% to 45% of the window height for improved layout and user experience.

--- resources/js/views/HomePage.vue
+++ resources/js/views/HomePage.vue
@@ -54,7 +54,7 @@ eslint-disable no-undef
                         <div class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                             <div class="w-full max-w-xl lg:shrink-0 xl:max-w-2xl sm:mt-10">
                                
-<h1 class="tracking-tight text-gray-300 sm:text-6xl 2xl:mt-10 mt-20 mb-4 text-4xl font-extrabold leading-none md:text-5xl lg:text-6xl dark:text-white">
+<h1 class="tracking-tight text-gray-300 sm:text-6xl 2xl:mt-10 mt-10 mb-4 text-4xl font-extrabold leading-none md:text-5xl lg:text-6xl dark:text-white">
   NM Technology is Albuquerque's Trusted Commercial CCTV, Security & Fire Systems Installation Experts, We Are Changing The Way New Mexicans are
   <span class="text-green-600 dark:text-blue-500">Protected and Served</span>Right Here in New Mexico!
 </h1>
@@ -182,7 +182,7 @@ export default {
 
     const showFixedBanner = ref(false)
     const handleScroll = () => {
-      showFixedBanner.value = window.scrollY > window.innerHeight * 0.40
+      showFixedBanner.value = window.scrollY > window.innerHeight * 0.45
     }
 
     onMounted(() => {
