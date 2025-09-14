5abd5f07c3c5f72f90269e73dfe1d269255de806
nmtechnology <patrick@nmtechnology.us>
2025-09-14 16:37:27 -0600

    Update HomePage parallax and layout styles
    Refactored the HomePage layout by adjusting the positioning and z-index of the DesertParallax and background containers for improved layering and visual structure.

--- resources/js/views/HomePage.vue
+++ resources/js/views/HomePage.vue
@@ -2,11 +2,10 @@ eslint-disable comma-dangle
 eslint-disable no-undef
 <template>
     <TopBanner />
-    <div class="relative z-10">
+    <div class="absolute top-0 left-0 w-full z-0">
       <DesertParallax />
-      <div class="absolute inset-0 pointer-events-none" style="height:100%;z-index:-1;"></div>
     </div>
-    <div class="bg-gray-900">
+    <div class="relative z-10 bg-gray-900">
         <main>
             <div class="relative isolate">
                 <svg class="absolute inset-x-0 top-0 -z-40 h-[84rem] w-full stroke-slate-600 [mask-image:radial-gradient(40rem_30rem_at_center,white,transparent)]"
