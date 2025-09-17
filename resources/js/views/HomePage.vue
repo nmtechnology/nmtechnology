d5763b8678ba692e6ec567a0b7ff35c454563e67
nmtechnology <patrick@nmtechnology.us>
2025-09-16 22:01:37 -0600

    Add 'fixed' class to SVG on HomePage
    The SVG element on the HomePage now includes the 'fixed' class in addition to 'overflow-visible', likely to adjust its positioning or behavior in the layout.

--- resources/js/views/HomePage.vue
+++ resources/js/views/HomePage.vue
@@ -59,7 +59,7 @@ eslint-disable no-undef
         <path d="M.5 200V.5H200" fill="none" />
       </pattern>
     </defs>
-    <svg x="50%" y="-1" class="overflow-visible">
+    <svg x="50%" y="-1" class="overflow-visible fixed">
       <path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
         stroke-width="0" />
     </svg>
