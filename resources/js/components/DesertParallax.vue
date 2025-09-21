3cd571ea8f286cadabc34ef1998cbe0ce3d1b53c
nmtechnology <patrick@nmtechnology.us>
2025-09-20 20:50:00 -0600

    Lower z-index of parallax container
    Changed the z-index from -10 to -20 for the parallax container to ensure it remains behind all other content.

--- resources/js/components/DesertParallax.vue
+++ resources/js/components/DesertParallax.vue
@@ -39,7 +39,7 @@ onMounted(() => {
   width: 100vw;
   height: 95vh;
   overflow: hidden;
-  z-index: -10; /* Use -1 to ensure the parallax is always behind all content */
+  z-index: -20; /* Use -1 to ensure the parallax is always behind all content */
   display: flex;
   align-items: center;
   justify-content: center;
