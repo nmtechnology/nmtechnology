d6275254539b21bd51e1353fdb6a008665449489
NM Technology <patrick@nmtechnology.us>
2025-11-04 15:27:04 -0700

    Fix heading structure and grammar on HomePage
    Moved the 'New Mexico' heading outside the main h1 for better semantic structure and corrected a grammatical error in the description text.

--- resources/js/views/HomePage.vue
+++ resources/js/views/HomePage.vue
@@ -279,8 +279,8 @@
               <div class="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                 <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">
                   <span class="text-white">We're changing the way you are</span> <span class="text-green-400">protected and served</span> <span class="text-white">Right Here In</span> <img class="w-16 h-16" src="/public/images/zia-symbol.webp" alt="New Mexico Logo"></img>
-                  <h1 class="text-yellow-400">New Mexico</h1>
-                </h1>
+                  
+                </h1><h1 class="text-yellow-400">New Mexico</h1>
                 <p
                   class="relative mt-6 text-lg leading-8 text-white sm:max-w-md lg:max-w-none"
                 >
@@ -290,7 +290,7 @@
                   of security services and products that address both physical and cyber
                   threats, we can protect organizations of all types and sizes protect
                   their people, assets, and data. In addition, we offer ongoing
-                  maintenance and support services to ensure that these systems continue
+                  maintenance and support services to ensure that these systems continues
                   to function effectively over time.
                 </p>
 
