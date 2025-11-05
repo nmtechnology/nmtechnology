debc173fc1a9464914b923d3374f17eb5aeead82
NM Technology <patrick@nmtechnology.us>
2025-11-05 09:58:24 -0700

    Update favicon size and title styling in welcome view
    Increased the favicon width from w-16 to w-20 and added italic, bold, and margin-left classes to the page title in welcome.blade.php.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/webp" href="{{ asset('images/nm-logo-rmbg.webp') }}" class="h-8 w-16">
+    <link rel="icon" type="image/webp" href="{{ asset('images/nm-logo-rmbg.webp') }}" class="h-8 w-20">
 
-    <title>Technology</title>
+    <title class="italic font-bold -ml-1">Technology</title>
 
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
