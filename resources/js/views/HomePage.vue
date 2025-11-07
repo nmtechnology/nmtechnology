ae3cf23a1032aedb152c9ad59cda779c76a10391
NM Technology <patrick@nmtechnology.us>
2025-11-06 21:26:01 -0700

    Update homepage tagline color to green
    Changed the tagline text color from red to green for improved visual consistency on the HomePage component.

--- resources/js/views/HomePage.vue
+++ resources/js/views/HomePage.vue
@@ -64,7 +64,7 @@
                       clip-rule="evenodd"
                     />
                   </svg>
-                  <span class="text-sm font-semibold text-red-400"
+                  <span class="text-sm font-semibold text-green-400"
                     >New Mexico's Trusted Security Experts</span
                   >
                 </div>
