ffad59a5e480eb1da5e39cbe476ab7db10f8a762
NM Technology <58670832+nmtechnology@users.noreply.github.com>
2026-01-05 23:48:18 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/webp" href="{{ asset('images/NM-logo-only.webp') }}" class="h-10">
+    <link rel="icon" type="image/webp" href="{{ asset('images/NM-logo-only.webp') }}" class="h-14 w-auto">
     <link rel="canonical" href="https://nmtechnology.us/">
 
     <!-- Primary SEO Meta Tags -->
