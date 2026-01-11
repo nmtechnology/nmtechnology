1f9651db2685b4a66fea614abfde71777a45842c
NM Technology <58670832+nmtechnology@users.noreply.github.com>
2026-01-11 12:02:10 -0700

    Update SecurityFAQsModal.vue

--- resources/js/components/SecurityFAQsModal.vue
+++ resources/js/components/SecurityFAQsModal.vue
@@ -10,7 +10,7 @@
 
     <teleport to="body">
       <div v-if="isOpen" class="modal" @click.self="closeModal">
-       
+        <div class="isolate bg-gray-900 modal-content">
           <div class="modal-header">
             <button class="text-white close-button" @click="closeModal">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
@@ -54,7 +54,7 @@
               </div>
             </div>
           </div>
-        
+        </div>
       </div>
     </teleport>
   </div>
